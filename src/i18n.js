89305be46c0d3ca3c04191e76eeffa2cc57bbab9
alfred91 <alfred.91.mc@gmail.com>
2024-09-24 16:44:50 +0200

    traduccion arreglada, update

--- src/i18n.js
+++ src/i18n.js
@@ -1,14 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector"; //DETECTA EL LENGUAJE DEL SISTEMA 
+import LanguageDetector from "i18next-browser-languagedetector";
 
 i18n
     .use(HttpBackend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        fallbackLng: "es",
+        fallbackLng: "en", // Idioma de respaldo
         debug: true,
         detection: {
             order: ["queryString", "cookie", "localStorage", "navigator"],
@@ -18,7 +18,16 @@ i18n
             escapeValue: false,
         },
         backend: {
-            loadPath: '/locales/{{lng}}/{{ns}}.json',
+            loadPath: '/locales/{{lng}}/{{ns}}.json', // Carga las traducciones
+        },
+        // Puedes agregar recursos directamente si no estás usando el backend
+        resources: {
+            es: {
+                translation: require('./locales/es/translation.json'), // Cargar español
+            },
+            en: {
+                translation: require('./locales/en/translation.json'), // Cargar inglés
+            },
         },
     });
 
